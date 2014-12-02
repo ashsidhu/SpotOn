772927c59a29d47ff1423959cf60be992e03e347
Ashmeet Sidhu <ashmeet.sidhu7@gmail.com>
2014-12-01 21:07:10 -0800

    post appointments from dashboard controller

--- client/app/dashboard/dashboard.controller.js
+++ client/app/dashboard/dashboard.controller.js
@@ -1,6 +1,35 @@
 'use strict';
 
 angular.module('spotOnApp')
-  .controller('DashboardCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('DashboardCtrl', function ($scope, $http) {
+    $scope.appointments = [];
+
+    $http.get('/api/appointments').success(function(appointments) {
+      $scope.appointments = appointments;
+    });
+
+    $scope.today = function () {
+      $scope.newAppointmentTimestamp = new Date();
+    };
+    $scope.today();
+
+    $scope.minDate = new Date();
+
+    $scope.setNextHour = function() {
+      $scope.newAppointmentTimestamp.setHours($scope.newAppointmentTimestamp.getHours()+1);
+      $scope.newAppointmentTimestamp.setMinutes(0);
+    };
+    $scope.setNextHour();
+
+    $scope.addAppointment = function() {
+      if(!$scope.newAppointmentTimestamp) {
+        return;
+      }
+      $http.post('/api/appointments', { dueDate: $scope.newAppointmentTimestamp });
+    };
+
+    $scope.deleteAppointment = function(appointment) {
+      $http.delete('/api/appointments/' + appointment._id);
+    };
+
   });
